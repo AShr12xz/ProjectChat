399530665433a6cbccaafb7db63eca66eda31b84
AShr12xz <122085103+AShr12xz@users.noreply.github.com>
2023-08-17 23:30:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+const chatLog = document.getElementById('chat-log'),
+    userInput = document.getElementById('user-input'),
+    sendButton = document.getElementById('send-button'),
+    buttonIcon = document.getElementById('button-icon'),
+    info = document.querySelector('.info');
+const API_KEY = "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"
+
+sendButton.addEventListener('click', sendMessage);
+userInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        sendMessage();
+    }
+});
+
+function sendMessage() {
+    const message = userInput.value.trim();
+    if (message === '') {
+        return;
+    }
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    appendMessage('user', message);
+    userInput.value = '';
+
+    const options = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content:message}],
+        })
+
+    };
+    
+    fetch(API_URL, options).then((response) => response.json())
+    .then((response) => {
+        if (response.choices && response.choices.length > 0) {
+            const botMessage = response.choices[0].message.content;
+            if (botMessage) {
+                appendMessage('bot', botMessage);
+            } else {
+                appendMessage('bot', 'Received an empty bot message.');
+            }
+        } else {
+            appendMessage('bot', 'No valid response from the bot.');
+        }
+
+        buttonIcon.classList.add('fa-solid', 'fa-paper-plane');
+        buttonIcon.classList.remove('fas', 'fa-spinner', 'fa-pulse');
+    })
+    .catch((err) => {
+        if (err.name === 'TypeError') {
+            appendMessage('bot', 'Error: Check Your API Key!');
+        } else {
+            appendMessage('bot', 'An error occurred while processing the response.');
+        }
+    });
+
+}
+
+function appendMessage(sender, message) {
+    info.style.display = "none";
+    // change send button icon to loading using fontawesome
+    buttonIcon.classList.remove('fa-solid', 'fa-paper-plane');
+    buttonIcon.classList.add('fas', 'fa-spinner', 'fa-pulse');
+
+    const messageElement = document.createElement('div');
+    const iconElement = document.createElement('div');
+    const chatElement = document.createElement('div');
+    const icon = document.createElement('i');
+
+    chatElement.classList.add("chat-box");
+    iconElement.classList.add("icon");
+    messageElement.classList.add(sender);
+    messageElement.innerText = message;
+
+    // add icons depending on who send message bot or user
+    if (sender === 'user') {
+        icon.classList.add('fa-regular', 'fa-user');
+        iconElement.setAttribute('id', 'user-icon');
+    } else {
+        icon.classList.add('fa-solid', 'fa-robot');
+        iconElement.setAttribute('id', 'bot-icon');
+    }
+
+    iconElement.appendChild(icon);
+    chatElement.appendChild(iconElement);
+    chatElement.appendChild(messageElement);
+    chatLog.appendChild(chatElement);
+    chatLog.scrollTo = chatLog.scrollHeight;
+
+} 
